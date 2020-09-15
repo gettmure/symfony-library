4f98680c8e666a29dabe601fabbb5f08d34fdcc3
gettmure <warcraft6500@gmail.com>
2020-09-15 18:17:40 +1000

    added twig support

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LibraryController {
+class LibraryController extends AbstractController {
 
     /**
      * @Route("/")
