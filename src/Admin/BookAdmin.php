c3d740466976fbd9f12cc8d7dbbe0599e7420b5c
gettmure <warcraft6500@gmail.com>
2020-09-18 16:13:45 +1000

    updated user and book dashboards

--- src/Admin/BookAdmin.php
+++ src/Admin/BookAdmin.php
@@ -3,6 +3,7 @@
 
 namespace App\Admin;
 
+use App\Entity\User;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -10,26 +11,42 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Sonata\AdminBundle\Form\Type\ModelType;
 
 final class BookAdmin extends AbstractAdmin {
+    /**
+     * @param FormMapper $formMapper
+     */
     protected function configureFormFields(FormMapper $formMapper) {
-        $formMapper->add('name', TextType::class);
-        $formMapper->add('year', IntegerType::class);
-        $formMapper->add('description', TextType::class);
-        $formMapper->add('imageUrl', UrlType::class);
+        $formMapper
+            ->with('Описание книги')
+            ->add('name', TextType::class)
+            ->add('year', IntegerType::class)
+            ->add('description', TextType::class)
+            ->add('imageUrl', UrlType::class)
+            ->end()
+            ->with('Авторы')
+            ->add('authors', ModelType::class, [
+                'class' => User::class,
+                'property' => 'username',
+                'multiple' => true,
+            ])
+            ->end();
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
-        $datagridMapper->add('name');
-        $datagridMapper->add('year');
-        $datagridMapper->add('description');
-        $datagridMapper->add('imageUrl');
+        $datagridMapper
+            ->add('name')
+            ->add('year')
+            ->add('description')
+            ->add('imageUrl');
     }
 
     protected function configureListFields(ListMapper $listMapper) {
-        $listMapper->addIdentifier('name');
-        $listMapper->addIdentifier('year');
-        $listMapper->addIdentifier('description');
-        $listMapper->addIdentifier('imageUrl');
+        $listMapper
+            ->addIdentifier('name')
+            ->addIdentifier('year')
+            ->addIdentifier('description')
+            ->addIdentifier('imageUrl');
     }
 }
--- src/Admin/UserAdmin.php
+++ src/Admin/UserAdmin.php
@@ -7,25 +7,40 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class UserAdmin extends AbstractAdmin {
     protected function configureFormFields(FormMapper $formMapper) {
-        $formMapper->add('username', TextType::class);
-        $formMapper->add('firstname', TextType::class);
-        $formMapper->add('lastname', TextType::class);
+        $formMapper
+            ->with('Информация о пользователе')
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->end();
     }
 
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
     protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
-        $datagridMapper->add('username');
-        $datagridMapper->add('firstname');
-        $datagridMapper->add('lastname');
+        $datagridMapper
+            ->add('username')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname');
     }
 
+    /**
+     * @param ListMapper $listMapper
+     */
     protected function configureListFields(ListMapper $listMapper) {
-        $listMapper->addIdentifier('username');
-        $listMapper->addIdentifier('firstname');
-        $listMapper->addIdentifier('lastname');
+        $listMapper
+            ->addIdentifier('username')
+            ->addIdentifier('password')
+            ->addIdentifier('firstname')
+            ->addIdentifier('lastname');
 
     }
 }
