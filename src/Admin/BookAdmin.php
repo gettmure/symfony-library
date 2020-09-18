b32dd5fc586253b3da50292b73966002768db821
gettmure <warcraft6500@gmail.com>
2020-09-18 15:41:03 +1000

    changed form types

--- src/Admin/BookAdmin.php
+++ src/Admin/BookAdmin.php
@@ -7,14 +7,16 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 
 final class BookAdmin extends AbstractAdmin {
     protected function configureFormFields(FormMapper $formMapper) {
         $formMapper->add('name', TextType::class);
-        $formMapper->add('year', TextType::class);
+        $formMapper->add('year', IntegerType::class);
         $formMapper->add('description', TextType::class);
-        $formMapper->add('imageUrl', TextType::class);
+        $formMapper->add('imageUrl', UrlType::class);
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
